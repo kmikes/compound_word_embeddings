62d122e076a6d19c8bb19e12267e9d1d966bbb00
kmikes <kaija.mikes@gmail.com>
2021-08-13 10:49:31 -0600

    add download-data.sh

---
+++ data/download-data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -C - -O http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip
+curl -o LADECv1-2019.csv https://era.library.ualberta.ca/items/dc3b9033-14d0-48d7-b6fa-6398a30e61e4/download/830937da-a00b-4735-8cf2-3c67d5cc6d50
